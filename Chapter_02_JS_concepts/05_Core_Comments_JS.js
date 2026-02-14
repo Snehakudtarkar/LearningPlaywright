68daee36eee26e789b35fef12af78a7444fba69b
Snehakudtarkar <snehakudtarka33@gmail.com>
2026-02-14 09:31:18 +0530

    Update JS concepts and basics

---
+++ Chapter_02_JS_concepts/05_Core_Comments_JS.js
@@ -0,0 +1,13 @@
+// This is comment. -This code will not be execute
+console.log("Hello");
+/** 
+ *  This is multi line
+ *  Author : Sneha Kudtarkar
+ *  Date : 14-Feb-2026
+ **/
+
+/* 
+ *  This is multi line
+ *  Author : Sneha Kudtarkar
+ *  Date : 14-Feb-2026
+ */
